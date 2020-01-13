2f50355c422009235690875e517fdd3c8881a817
mumu2117 <jmillerarcher@me.comgit config --global user.name mumu2117>
2020-01-12 21:05:20 -0500

    first commit of sample project "Just sample"

---
+++ lib/pet_adoptions.rb
@@ -0,0 +1,6 @@
+require "pet_adoptions/version"
+
+module PetAdoptions
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pet_adoptions/version.rb
@@ -0,0 +1,3 @@
+module PetAdoptions
+  VERSION = "0.1.0"
+end
